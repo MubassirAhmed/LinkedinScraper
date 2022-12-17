6e5de86f997d1a510f81042b05b6b19b5a6778d2
MubassirAhmed <mubassirahmed01@gmail.com>
2022-12-16 19:39:49 -0500

    for loop fixed, so now it iterates over multiple linkedin searches

--- linkedin/linkedin/spiders/linkedinSpider.py
+++ linkedin/linkedin/spiders/linkedinSpider.py
@@ -1,126 +1,118 @@
 import scrapy
 from scrapy.http import TextResponse
 import requests
+import re
 
 class linkedinSpider(scrapy.Spider):
+    
     name = "linkedin"
+    
+    custom_settings = {
+        'FEEDS': { "s3://linkedin-scraper-1/%(name)s/%(name)s_%(time)s.csv" : {"format": "csv"} }
+    }
 
-    def start_requests(self):
-        #url = "https://www.linkedin.com/jobs/search/?currentJobId=3335051477&geoId=105149290&location=Ontario%2C%20Canada&start={}"
-        
+    
+    def start_requests(self):              
         #? Real Jobs
-        _SQL_Entry_RemoteCanada_24hrs ='https://www.linkedin.com/jobs-guest/jobs/api/seeMoreJobPostings/search?keywords=SQL&location=Canada&locationId=&geoId=101174742&f_TPR=r86400&f_WT=2&position=1&pageNum=0&start={}'
-        SQL_Any_RemoteCanada_24hrs ='x'
-        
-        _SQL_Entry_AnyToronto_24hrs = 'x'
-        SQL_Any_AnyToronto_24hrs = 'x'
+        SQL_RemoteCanada_24hrs ='https://ca.linkedin.com/jobs/search?keywords=SQL&location=Canada&locationId=&geoId=101174742&f_TPR=r86400&f_WT=2&position=1&pageNum=0'
+        SQL_Toronto_24hrs = 'https://ca.linkedin.com/jobs/search?keywords=SQL&location=Toronto%2C%20Ontario%2C%20Canada&locationId=&geoId=100761630&f_TPR=r86400&distance=25&position=1&pageNum=0'
+        Python_RemoteCanada_24hrs = 'https://ca.linkedin.com/jobs/search?keywords=Python&location=Canada&locationId=&geoId=101174742&f_TPR=r86400&f_WT=2&position=1&pageNum=0'
+        Python_Toronto_24hrs = 'https://ca.linkedin.com/jobs/search?keywords=Python&location=Toronto%2C%20Ontario%2C%20Canada&locationId=&geoId=100025096&f_TPR=r86400&position=1&pageNum=0'
+        Econ_RemoteCanada_24hrs = 'https://ca.linkedin.com/jobs/search?keywords=Economics&location=Canada&locationId=&geoId=101174742&f_TPR=&f_WT=2&position=1&pageNum=0'
+        Econ_Toronto_24hrs ='https://ca.linkedin.com/jobs/search?keywords=Economics&location=Toronto%2C%20Ontario%2C%20Canada&locationId=&geoId=100025096&f_TPR=r86400&position=1&pageNum=0'
+        SQL_RemoteUS_24hrs = 'https://ca.linkedin.com/jobs/search?keywords=Sql&location=United%20States&locationId=&geoId=103644278&f_TPR=r86400&f_WT=2&f_E=2&position=1&pageNum=0'
         
         #? Any Jobs
-        _Any_Entry_RemoteCanada_24hrs = 'https://ca.linkedin.com/jobs/search?keywords=&location=Canada&locationId=&geoId=101174742&f_TPR=r86400&f_E=2&f_WT=2&position=1&pageNum=0'
+        #'support','associate','specialist'
+        Entry_RemoteCanada_24hrs = 'https://ca.linkedin.com/jobs/search?keywords=&location=Canada&locationId=&geoId=101174742&f_TPR=r86400&f_E=2&f_WT=2&position=1&pageNum=0'
         Any_Any_RemoteCanada_24hrs = 'https://ca.linkedin.com/jobs/search?keywords=&location=Canada&locationId=&geoId=101174742&f_TPR=r86400&f_WT=2&position=1&pageNum=0'
         
         _Any_Entry_AnyToronto_24hrs = 'x'
-        Any_Any_AnyToronto_24hrs = 'x'
+        Any_Any_AnyToronto_24hrs = 'https://ca.linkedin.com/jobs/search?keywords=&location=Toronto%2C%20Ontario%2C%20Canada&locationId=&geoId=100025096&f_TPR=r86400&position=1&pageNum=0'
 
-        feederURL = Any_Any_RemoteCanada_24hrs
-        
-        #? Do entry roles for trial runs, but in actual runs, scan everything
-        
-        #TODO Today: 
+        #TODO : 
         #? Scraper Stuff:
-        #? Figure out how to scrape number of jobs from initial link and feed it in to the scraper
-        #? Figure out trial runs
-        #? Figure out final runs
+        #? add no.ofApps, and timePosted to the results/json
+        """ #? improve consistency, check error.log in spiders folder for 
+        the last real (i.e., not test) run """        
         
         #? Figure out how to analyse this in jupyter notebooks
-        #! obv filter for no to 0 experience - figure this out in kijiji analysis book
-        #? apps/hour calc.
         #? common key-words
-
-        
         #? filter by less apps to more apps
-        
-        
-        #total_jobs = requests.get(
-       #     'https://www.linkedin.com/jobs/search?keywords=SQL&location=Canada&locationId=&geoId=101174742&f_TPR=r86400&f_WT=2&position=1&pageNum=0')
-        #total_jobs_r1 = TextResponse(body=total_jobs.content, url=url)
-        #total_jobs_resp = total_jobs_r1.css('span.results-context-header__job-count::text').extract()
-        #clean_total_jobs = int(" ".join(total_jobs_resp).strip().replace("+", "").replace(",", ""))
-        totalJobs = int(TextResponse(body=requests.get(feederURL).content, url=feederURL).css('span.results-context-header__new-jobs::text').get().strip().replace("\xa0new",'').replace("(","").replace(',','').replace(')',''))
+        #? do analysis dashboards like the ones the fiver guys had
+        feederURLs = [SQL_RemoteCanada_24hrs,SQL_Toronto_24hrs,
+                      Python_RemoteCanada_24hrs,Python_Toronto_24hrs,
+                      Econ_RemoteCanada_24hrs,Econ_Toronto_24hrs,
+                      SQL_RemoteUS_24hrs]
+        #feederURL = SQL_RemoteCanada_24hrs    
+                    
+        for feederURL in feederURLs: 
+            totalJobs = int(TextResponse(body=requests.get(feederURL)\
+                .content, url=feederURL)\
+                .css('span.results-context-header__new-jobs::text')\
+                .get().strip().replace("\xa0new",'').replace("(","")\
+                .replace(',','').replace(')',''))
 
-        #? if there are 402 jobs, the page will load if you pass '400' as the parameter, but not '425'. However, it loads if you put '402', so this way you can get the last few jobs. You can put a second for loop just for those last few, but you may need another parse func for that.
-        for i in range(0, totalJobs, 25):
-            yield scrapy.Request(url=feederURL.replace("/jobs/", "/jobs-guest/jobs/api/seeMoreJobPostings/") + "&start={}".format(i),  # meta = {
-                                 #   'dont_redirect': True,
-                                 #   'handle_httpstatus_list': [303]},
-                                 callback=self.after_fetch)
+            """if there are 402 jobs, the page will load if you pass '400' 
+            as the parameter, but not '425'. However, it loads if you put '402',
+            so this way you can get the last few jobs. You can put a second 
+            for loop just for those last few, but you may need another parse func 
+            for that."""
+            
+            for i in range(0, totalJobs, 25):
+                yield scrapy.Request(url=feederURL.replace("/jobs/",
+                                        "/jobs-guest/jobs/api/seeMoreJobPostings/") 
+                                        + "&start={}".format(i),
+                                    callback=self.after_fetch)
         
 
     def after_fetch(self, response):
-        # dates_list = response.xpath('//time[@class="job-search-card__listdate"]/@datetime').getall()
         job_link = response.css('a.base-card__full-link::attr(href)').extract()
 
         for link in job_link:
             yield response.follow(url=link,
-                                  callback=self.parse)  # ,
-            # meta={'dates_list':dates_list})
+                                  callback=self.parse) 
 
     def parse(self, response, **kwargs):
-        #job_desc = response.css('div.show-more-less-html__markup ::text').extract()
-        clean_desc = " ".join(response.css('div.show-more-less-html__markup ::text').extract()).strip().lower()
-        
-        #criterion = response.css('span.description__job-criteria-text::text').get().strip().lower()
-        clean_criterion = response.css('span.description__job-criteria-text::text').get().strip().lower()
-        
-        postedTimeAgo =  response.css('span.posted-time-ago__text::text').get().strip().lower()
-        
+        postedTimeAgo =  response.css('span.posted-time-ago__text::text')\
+            .get().strip().lower()
         if any(word in postedTimeAgo for word in ['hour','hours']):
-            postedTimeAgo = int(postedTimeAgo.replace("hours ago",'').replace("hour ago",'').strip())
+            postedTimeAgo = int(postedTimeAgo.replace("hours ago",'')\
+            .replace("hour ago",'').strip())
         else:
-        
             if any(word in postedTimeAgo for word in ['minutes','minute']):
-                postedTimeAgo = int(postedTimeAgo.replace("minute ago",'').replace("minutes ago",'').strip())/60
+                postedTimeAgo = int(postedTimeAgo.replace("minute ago",'')\
+                .replace("minutes ago",'').strip())/60
             else: 
                 if any(word in postedTimeAgo for word in ['day','days']):
-                    postedTimeAgo = int(postedTimeAgo.replace("day ago",'').replace("days ago",'').strip())*24
-    
-            
-        
-        noApplicants = response.css('.num-applicants__caption::text').get().strip().lower()
+                    postedTimeAgo = int(postedTimeAgo.replace("day ago",'')\
+                    .replace("days ago",'').strip())*24
+                    
+        noApplicants = response.css('.num-applicants__caption::text').get()\
+            .strip().lower()
         if 'among' in noApplicants:
             noApplicants = 0
         else:
-            noApplicants = int(noApplicants.replace("applicants",'').replace("over",''))
+            noApplicants = int(noApplicants.replace("applicants",'')\
+                .replace("over",''))   
             
         appsPerHr = noApplicants/postedTimeAgo
-        #job_title = response.css('h1.topcard__title::text').get()
-        clean_title = response.css('h1.topcard__title::text').get().strip().lower()
-         
-        #company = response.css('a.topcard__org-name-link::text').get()
-        clean_company = response.css('a.topcard__org-name-link::text').get().strip().lower()
+        clean_desc = " ".join(response\
+            .css('div.show-more-less-html__markup ::text')\
+            .extract()).strip().lower()  
+        clean_title = response.css('h1.topcard__title::text').get().strip()\
+            .lower()
+        clean_company = response.css('a.topcard__org-name-link::text').get()\
+            .strip().lower()
+        clean_type_of_job = response\
+            .css('span.description__job-criteria-text::text').get().strip()\
+            .lower()
+        job_link = response.request.url
+        job_id = int(re.findall("\d{10}",job_link)[0])
         
-        #type_of_job = response.css('span.description__job-criteria-text::text').get()
-        clean_type_of_job = response.css('span.description__job-criteria-text::text').get().strip().lower()
-        
-        #location = response.css('span.topcard__flavor::text').get()
-        #clean_loc = "".join(response.css('span.topcard__flavor::text').getall()).strip().replace(",", "|")
-                
-
-        #dates_list = response.meta.get('dates_list')
-        
-        #industry = response.css('span.description__job-criteria-text::text').get()
-        clean_industry = response.css('span.description__job-criteria-text::text').get().strip().lower()
-
-        # try:
-        #   date_posted = dates_list[0]
-        #  dates_list.pop(0)
-        # except:
-        #   pass
-
-        yield {'title': clean_title, 'appsPerHour': appsPerHr, 'typeOfJob': clean_type_of_job, 'job_link': response.request.url,
-                'company': clean_company, 'industry': clean_industry, 'description': clean_desc,
-                'criterion': clean_criterion} #, 'location': clean_loc, 'time_posted': clean_time}  # ,
-        # 'datePosted':date_posted}
-
-        #yield all_items
-
+        yield {'title': clean_title, 'appsPerHour': appsPerHr, 
+               'noApplicants': noApplicants,'postedTimeAgo':postedTimeAgo,
+                'company': clean_company,'job_link': job_link,
+                'description': clean_desc, 'typeOfJob': clean_type_of_job,
+                'job_id': job_id
+                }
